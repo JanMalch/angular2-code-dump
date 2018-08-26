3a45c2dfdd1e1c9d40aecaba77376d47c45285f5
JanJan <janmalch@gmail.com>
2018-08-26 16:50:24 +0200

    Added ngSubscribe directive

---
+++ custom directives/ng-subscribe.directive.ts
@@ -0,0 +1,53 @@
+import {
+  Directive,
+  Input,
+  ViewContainerRef,
+  TemplateRef,
+  OnInit,
+  OnDestroy,
+  ChangeDetectorRef
+} from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
+export class NgSubscribeContext {
+  public ngSubscribe: any = null;
+}
+
+// ALL CREDIT TO Netanel Basal
+// https://netbasal.com/diy-subscription-handling-directive-in-angular-c8f6e762697f
+// Don't forget to add NgSubscribeDirective to your module's declarations!
+
+@Directive({
+  selector: '[ngSubscribe]'
+})
+export class NgSubscribeDirective implements OnInit, OnDestroy {
+  private observable: Observable<any>;
+  private context: NgSubscribeContext = new NgSubscribeContext();
+  private subscription: Subscription;
+
+  @Input()
+  set ngSubscribe(inputObservable: Observable<any>) {
+    if (this.observable !== inputObservable) {
+      this.observable = inputObservable;
+      this.subscription && this.subscription.unsubscribe();
+      this.subscription = this.observable.subscribe(value => {
+        this.context.ngSubscribe = value;
+        this.cdr.markForCheck();
+      });
+    }
+  }
+
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private cdr: ChangeDetectorRef,
+    private templateRef: TemplateRef<any>
+  ) {}
+
+  ngOnInit() {
+    this.viewContainer.createEmbeddedView(this.templateRef, this.context);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription && this.subscription.unsubscribe();
+  }
+}
